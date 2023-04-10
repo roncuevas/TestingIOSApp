5e57978078941347faa326c8735168b2c05d89ad
Aarón Martínez Cuevas <ronctm@gmail.com>
2023-04-10 12:59:46 -0600

    First commit

---
+++ TestingIOSApp/TestingIOSApp/TestingIOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingIOSAppApp.swift
+//  TestingIOSApp
+//
+//  Created by Aarón Alberto Martínez Cuevas on 10/04/23.
+//
+
+import SwiftUI
+
+@main
+struct TestingIOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

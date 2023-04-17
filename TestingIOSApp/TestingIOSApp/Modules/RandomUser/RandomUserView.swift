27b19b9fc63d90da9c09b4ae11243ce520082a81
Aarón Martínez Cuevas <ronctm@gmail.com>
2023-04-17 08:33:02 -0600

    UPDATE: RandomUser, modifiying the saved users view

--- TestingIOSApp/TestingIOSApp/Modules/RandomUser/RandomUserView.swift
+++ TestingIOSApp/TestingIOSApp/Modules/RandomUser/RandomUserView.swift
@@ -18,60 +18,54 @@ struct RandomUserView: View {
     var body: some View {
         VStack {
             ScrollView {
-                VStack() {
+                VStack {
+                    Spacer()
+                    KFImage(URL(string: viewModel.picture))
+                        .placeholder({ _ in
+                            LottieView(animationFinished: $imageLoading, name: "loader3", loopMode: .loop)
+                                .frame(width: 70, height: 70)
+                        })
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                        .clipShape(Circle())
+                    Spacer()
                     Text(viewModel.fullName)
                         .font(.system(size: 24))
                         .fontWeight(.bold)
                         .padding(.bottom, 4)
-                    HStack {
-                        KFImage(URL(string: viewModel.picture))
-                            .placeholder({ _ in
-                                LottieView(animationFinished: $imageLoading, name: "loader3", loopMode: .loop)
-                                    .frame(width: 70, height: 70)
-                            })
-                            .resizable()
-                            .frame(width: 100, height: 100)
-                            .clipShape(Circle())
-                        Text("Hello world, we're testing this header so keep it up with the new updates of our app.")
-                            .padding(.leading, 16)
-                    }
-                    .padding([.leading, .trailing], 16)
-                    HStack {
-                        Button {
-                            viewModel.getRandomUser()
-                        } label: {
-                            Text("New")
-                                .frame(minWidth: 0, maxWidth: .infinity)
-                                .fontWeight(.bold)
-                                .foregroundColor(.white)
-                                .padding()
-                                .background(
-                                    RoundedRectangle(cornerRadius: 10)
-                                        .foregroundColor(.accentColor)
-                                )
-                                .cornerRadius(25)
-                        }
-                        .buttonStyle(.plain)
-                        Button {
-                            viewModel.saveRandomUser()
-                        } label: {
-                            Text("Save")
-                                .frame(minWidth: 0, maxWidth: .infinity)
-                                .fontWeight(.bold)
-                                .foregroundColor(.white)
-                                .padding()
-                                .background(
-                                    RoundedRectangle(cornerRadius: 10)
-                                        .foregroundColor(.accentColor)
-                                )
-                                .cornerRadius(25)
-                        }
-                        .buttonStyle(.plain)
-                    }
+                    Spacer()
+                    Text("Hello world, we're testing this header so keep it up with the new updates of our app.")
+                        .padding(.leading, 16)
+                    Spacer()
+                }
+                .padding([.leading, .trailing], 16)
+
+            }
+            .refreshable {
+                viewModel.getRandomUser()
+            }
+            Spacer()
+            VStack {
+                HStack {
                     Button {
-                        navigationManager.path.append(NavigationRoute.randomUserSavedView)
+                        viewModel.getRandomUser()
                     } label: {
-                        Text("Saved users")
+                        Text("New")
+                            .frame(minWidth: 0, maxWidth: .infinity)
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .padding()
+                            .background(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .foregroundColor(.accentColor)
+                            )
+                            .cornerRadius(25)
+                    }
+                    .buttonStyle(.plain)
+                    Button {
+                        viewModel.saveRandomUser()
+                    } label: {
+                        Text("Save")
                             .frame(minWidth: 0, maxWidth: .infinity)
                             .fontWeight(.bold)
                             .foregroundColor(.white)
@@ -84,15 +78,29 @@ struct RandomUserView: View {
                     }
                     .buttonStyle(.plain)
                 }
-                .padding(16)
-                .addLoader(show: $viewModel.isLoading, frame: CGSize(width: 200, height: 200))
-                .navigationTitle(viewModel.fullName)
-                .onAppear {
-                    viewModel.getRandomUser()
+                Button {
+                    navigationManager.path.append(NavigationRoute.randomUserSavedView)
+                } label: {
+                    Text("Saved users")
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(
+                            RoundedRectangle(cornerRadius: 10)
+                                .foregroundColor(.accentColor)
+                        )
+                        .cornerRadius(25)
                 }
+                .buttonStyle(.plain)
             }
         }
-        .frame(maxWidth: .infinity)
+        .padding(16)
+        .addLoader(show: $viewModel.isLoading, frame: CGSize(width: 200, height: 200))
+        .navigationTitle(viewModel.fullName)
+        .onAppear {
+            viewModel.getRandomUser()
+        }
     }
 }
 

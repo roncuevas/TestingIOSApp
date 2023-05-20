64f6ec4c5362ba7fdc1a586de7b37180a795113b
Aarón Martínez Cuevas <ronctm@gmail.com>
2023-05-20 09:20:17 -0600

    FOLDERS: Sorting folders

--- TestingIOSApp/TestingIOSApp/Utils/Network/AsyncNetworkCall.swift
+++ TestingIOSApp/TestingIOSApp/Utils/Network/AsyncNetworkCall.swift
@@ -6,23 +6,36 @@
 //
 
 import Foundation
+import SwiftUI
 
 enum NetworkError: Error {
-    case wrongURL
+    case badURL
     case badResponse
+    case badDecoding
 }
 
 class AsyncNetworkCall {
     
     func executeQuery<T: Codable>(endpoint: String, type: T.Type) async throws -> T? {
         let url: URL? = URL(string: endpoint)
-        guard let url else { throw NetworkError.wrongURL }
+        guard let url else { throw NetworkError.badURL }
         do {
             let (data, response) = try await URLSession.shared.data(from: url)
             let httpResponse = response as? HTTPURLResponse
             guard httpResponse?.statusCode == 200 else { throw NetworkError.badResponse }
             let parsedData = try JSONDecoder().decode(type.self, from: data)
             return parsedData
+        } catch let error {
+            throw error
+        }
+    }
+    
+    func getImage(from url: String) async throws -> Image {
+        guard let url = URL(string: url) else { throw NetworkError.badURL }
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            guard let image = UIImage(data: data) else { throw NetworkError.badDecoding }
+            return Image(uiImage: image)
         } catch let error {
             throw error
         }
